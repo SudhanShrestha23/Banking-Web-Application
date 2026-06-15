d04ee0889e69b63c1f59bd1e8a8f57690b90c1d5
Sudhan Shrestha <sudhanshrestha25@gmail.com>
2026-06-15 20:51:43 +0800

    Added database config to backend

--- Banking_App/backend/settings.py
+++ Banking_App/backend/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'database.apps.DatabaseConfig'
+    'database.apps.DatabaseConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
